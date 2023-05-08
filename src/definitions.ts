c85929a13387ce9cdcd1b11f2cf7601ed3ad6557
Carsten Klaffke <mail@carsten-klaffke.de>
2023-05-08 16:53:34 +0200

    Capacitor 5 Upgrade

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    BillingPlugin: BillingPluginPlugin;
-  }
-}
-
 export interface BillingPluginPlugin {
   querySkuDetails(options: {product: string, type: string}): Promise<{value: string}>;
   launchBillingFlow(options: {product: string, type: string}): Promise<{value: string}>;
