f8b4ca7e96f683d577ad9d5f358965fd5fd2290b
Carsten Klaffke <mail@carsten-klaffke.de>
2025-07-22 13:22:59 +0200

    Upgrade to new BillingClient Version: com.android.billingclient:billing:7.1.0

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,3 +1,7 @@
-export * from './definitions';
-export * from './web';
+import { registerPlugin } from "@capacitor/core";
+const BillingPlugin = registerPlugin("BillingPlugin", {
+    web: () => import("./web").then((m) => new m.BillingPluginWeb()),
+});
+export * from "./definitions";
+export { BillingPlugin };
 //# sourceMappingURL=index.js.map
